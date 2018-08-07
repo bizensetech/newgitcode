5f4fea5b7b97081d7f2c6e5600089859e5d8e438
[shiva] <siva@bizense.com>
2018-08-07 18:29:05 +0530

    adx is added the agentconfig

--- agentconfig.js
+++ agentconfig.js
@@ -5,12 +5,18 @@ var router = express.Router();
 var mopub = require('./mopub_exchangeconnector');
 var smaato = require('./smaato_exchangeconnector');
 var pubnative = require('./pubnative_exchangeconnector');
+var adx = require('./adx_exchangeconnector');
+
 var mopubcheck = mopub.isCampaignValid;           
 var smaatocheck = smaato.isCampaignValid;
 var pubnativecheck = pubnative.isCampaignValid;
+var adxcheck = adx.isCampaignValid;
+
 var smaatodeleteId = smaato.deleteId;
 var mopubdeleteId = mopub.deleteId;
 var pubnativedeleteId = pubnative.deleteId;
+var adxdeleteId = adx.deleteId;
+
 var Agentconfig = {};
 
 router.post('/delete/:id', function(req, res, id){          // to delete the campaign
@@ -18,6 +24,7 @@ router.post('/delete/:id', function(req, res, id){          // to delete the cam
   smaatodeleteId(req.params.id);
   mopubdeleteId(req.params.id);
   pubnativedeleteId(req.params.id);
+  adxdeleteId(req.params.id);
   res.send('agentconfig deleted : '+ req.params.id);  
 });
 
@@ -42,6 +49,9 @@ router.post('/delete/:id', function(req, res, id){          // to delete the cam
     if(config.providerConfig.pubnative){                // if request is from pubnative
       pubnativecheck(req.params.id, config);
       }
+    if(config.providerConfig.adx){                      // if request is from adx
+      adxcheck(req.params.id, config);                  
+    }  
       res.send('agentconfig received');                 // sending response
   });
 
